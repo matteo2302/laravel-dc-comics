236cad28a6632527dc285066bee5eb564d49f8eb
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-29 12:41:04 +0200

    tried by adding the missing parts

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -9,6 +9,26 @@
         <label for="exampleInputPassword1" class="form-label">descrtiption</label>
         <input type="text" class="form-control" name="description" id="exampleInputPassword1">
     </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">thumb</label>
+        <input type="text" class="form-control" name="description" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">price</label>
+        <input type="number" class="form-control" name="price" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">series</label>
+        <input type="text" class="form-control" name="series" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">sale date</label>
+        <input type="date" class="form-control" name="sale_date" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">type</label>
+        <input type="text" class="form-control" name="type" id="exampleInputPassword1">
+    </div>
     <button type="submit" class="btn btn-primary">Add</button>
 </form>
 <button><a href="{{ route('comics.index') }}">home</a></button>
